78d9947dae79dc2292e4b6d38149abe2af9bd74c
Daniil Aksenov <aksenoff.dany@yandex.ru>
2025-12-25 01:39:09 +0300

    add alembic migrations for sqlite db

--- service/repo.py
+++ service/repo.py
@@ -30,7 +30,8 @@ class User(Base):
     def __repr__(self):
         return f"User(id={self.id}, username={self.username}, role={self.role})"
 
-Base.metadata.create_all(engine)
+# Tables will be created via Alembic migrations
+# Base.metadata.create_all(engine)
 
 def add_prediction(input, prediction):
     session = Session()
