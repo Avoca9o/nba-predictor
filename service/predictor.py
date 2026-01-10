204eef03ca41b08eae3b9fd6cf9f4c8fe2c7d735
Dmitry Vladimirov <kavvabanga@ya.ru>
2026-01-10 22:00:26 +0300

    fix service

--- service/predictor.py
+++ service/predictor.py
@@ -1,21 +1,34 @@
 import pandas as pd
 import pickle
 
-preprocessor = None
+abbr_id_mapper = None
+current_data = None
 predictor = None
 
-with open('preprocessor.pkl', 'rb') as f:
-    preprocessor = pickle.load(f)
+with open('abbr_id_mapper.csv', 'r') as f:
+    abbr_id_mapper = pd.read_csv(f)
+    abbr_id_mapper = dict(zip(abbr_id_mapper['abbreviation'], abbr_id_mapper['id']))
+
+with open('current_data.csv', 'r') as f:
+    current_data = pd.read_csv(f)
+
 with open('predictor.pkl', 'rb') as f:
     predictor = pickle.load(f)
 
 def get_prediction(data: dict):
-    columns = data.keys()
+    team_abbreviation_home = data['home_team_abbreaviation']
+    team_abbreviation_away = data['away_team_abbreaviation']
 
-    df = pd.DataFrame([data])
+    print(abbr_id_mapper.keys())
 
-    df = preprocessor.transform(df)
-    df = pd.DataFrame(df, columns=columns)
+    team_id_home = abbr_id_mapper[team_abbreviation_home]
+    team_id_away = abbr_id_mapper[team_abbreviation_away]
 
-    prediction = predictor.predict(df)
-    return int(prediction[0])
+    mask = (current_data['home_team_id'] == team_id_home) & (current_data['away_team_id'] == team_id_away)
+    if mask.any():
+        last_row = current_data[mask].iloc[[-1]]
+        last_row = last_row.drop(columns=['home_team_id', 'away_team_id'])
+        prediction = predictor.predict(last_row)
+        return int(prediction)
+    else:
+        return -1
